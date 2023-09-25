16fef9e85cf47720b53dfa96e35e104f5297fecb
Andrey-Kachow <andr10xp@gmail.com>
2023-09-25 22:27:30 +0100

    Added ability to view my CV

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,6 +1,18 @@
 import os
 
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+    Response,
+    make_response,
+    send_file
+)
+
+
+CV_PDF_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'documents')
+CV_PDF_PATH = os.path.join(CV_PDF_DIR, 'CV.pdf')    
+
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -9,4 +21,11 @@ def create_app():
     def index():
         return render_template('index.html')
 
+    @app.route('/cv')
+    def cv():
+        response = make_response(send_file(CV_PDF_PATH))
+        response.headers['Content-Type'] = 'application/pdf'
+        return response
+
     return app
+
