e392b9383f0680b415e27b3d18773df762a7bf4d
Andrey-Kachow <andr10xp@gmail.com>
2023-09-30 19:28:48 +0300

    Unified the work experience. Made sample todo-filling dictionaries

--- src/main/experiences.py
+++ src/main/experiences.py
@@ -1,5 +1,10 @@
+from flask import url_for
+
+
 SAMPLE_EXPERIENCE = {
     "class_prefix": "testpr",
+    "template_name": "abstract_experience.html",
+    "stylesheet_name": "abstract_experience.css",
     "reference_url": "#",
     "logo_path": "#",
     "title": {
@@ -17,3 +22,110 @@ SAMPLE_EXPERIENCE = {
     ]
 }
 
+
+MINIMAX_LABS = {
+    "class_prefix": "testpr",
+    "template_name": "abstract_experience.html",
+    "stylesheet_name": "abstract_experience.css",
+    "reference_url": "#",
+    "logo_path": "#",
+    "title": {
+        "prefix": "Software Engineering",
+        "highlight": "Pro",
+    },
+    "duration": "100 yrs",
+    "start_date": "31/10/1999",
+    "end_date": "31/10/2099",
+    "responsibilities": [
+        "Was cool and fun",
+        "Did interesting projects",
+        "Something sample third",
+        "Mo idea what is supposed to be here"
+    ]
+}
+
+
+IMPERIAL_PPT = {
+    "class_prefix": "testpr",
+    "template_name": "abstract_experience.html",
+    "stylesheet_name": "abstract_experience.css",
+    "reference_url": "#",
+    "logo_path": "#",
+    "title": {
+        "prefix": "Software Engineering",
+        "highlight": "Pro",
+    },
+    "duration": "100 yrs",
+    "start_date": "31/10/1999",
+    "end_date": "31/10/2099",
+    "responsibilities": [
+        "Was cool and fun",
+        "Did interesting projects",
+        "Something sample third",
+        "Mo idea what is supposed to be here"
+    ]
+}
+
+
+REPETITOR_RU = {
+    "class_prefix": "testpr",
+    "template_name": "abstract_experience.html",
+    "stylesheet_name": "abstract_experience.css",
+    "reference_url": "#",
+    "logo_path": "#",
+    "title": {
+        "prefix": "Software Engineering",
+        "highlight": "Pro",
+    },
+    "duration": "100 yrs",
+    "start_date": "31/10/1999",
+    "end_date": "31/10/2099",
+    "responsibilities": [
+        "Was cool and fun",
+        "Did interesting projects",
+        "Something sample third",
+        "Mo idea what is supposed to be here"
+    ]
+}
+
+
+SAMPLE_EXPERIENCE = {
+    "class_prefix": "testpr",
+    "template_name": "abstract_experience.html",
+    "stylesheet_name": "abstract_experience.css",
+    "reference_url": "#",
+    "logo_path": "#",
+    "title": {
+        "prefix": "Software Engineering",
+        "highlight": "Pro",
+    },
+    "duration": "100 yrs",
+    "start_date": "31/10/1999",
+    "end_date": "31/10/2099",
+    "responsibilities": [
+        "Was cool and fun",
+        "Did interesting projects",
+        "Something sample third",
+        "Mo idea what is supposed to be here"
+    ]
+}
+
+
+WORK_EXPERIENCES = [MINIMAX_LABS, IMPERIAL_PPT, REPETITOR_RU]
+# EDUCATION_EXPERIENES = [IMPERIAL, ASHBOURNE, WELLINGTON?]
+# RESEARCH_EXPERIENCE = [ALGOWIKI_ARTICLE]
+# ACADEMIC_PROJECTS = [EXPREME_RESTARTUP, MMMBOXES, WACC, DEVOPS_PROJECT, PINTOS, ARM11_EMULATOR]
+'''
+PET_PROJECTS = [
+    # TIC_TAC_TOE_ULTIMATE_ANDROID, # not yet started
+    # TIC_TAC_TOE_CPP, # not yet started
+    APOPOV_WEBSITE, 
+    PADAVAN_PY, 
+    HEALTH_RECIPES, 
+    KCAL_CALC,
+    BLUE_NOTES, 
+    NEA_FRESH_LIFE
+    BLOB_OF_FURY,
+]
+'''
+# TUTEES?
