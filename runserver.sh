7ba95bad64bd2ab6dbc5fb10fdfe440187d0dc2c
Andrey-Kachow <andr10xp@gmail.com>
2024-05-17 04:15:14 +0100

    Added remainder commands for windows setup

--- runserver.sh
+++ runserver.sh
@@ -3,3 +3,11 @@
 export FLASK_APP=src/main
 export FLASK_ENV=development
 flask run -h localhost -p 5001
+
+## Windows
+#
+# $Env:FLASK_APP = 'src/main'
+# $Env:FLASK_ENV = 'development'
+# flask run -h localhost -p 5001 --debug
+#    or
+# python -m flask run -h localhost -p 5001 --debug
