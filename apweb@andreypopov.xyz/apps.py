fb4d0156c9cd64423b614122c71de08b7ed29bf8
Andrey-Kachow <andr10xp@gmail.com>
2024-09-16 22:20:32 +0100

    Removed accidental junk

--- apweb@andreypopov.xyz/apps.py
+++
@@ -1,9 +0,0 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
-)
-
-bp = Blueprint('apps', __name__, url_prefix='/apps')
-
-@bp.route('/ntictactoe')
-def ntictactoe():
-    return render_template('apps/ntic_tac_toe/ntictactoe.html')
--- apweb@andreypopov.xyz/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Paths:
-    MAIN = os.path.dirname(os.path.abspath(__file__))
-    DOCUMENTS_DIR = os.path.join(MAIN, 'static', 'documents')
-    CV_PDF_PATH = os.path.join(DOCUMENTS_DIR, 'CV.pdf')    
-    FRESHIFE_PDF_PATH = os.path.join(DOCUMENTS_DIR, 'freshlife_writeup.pdf')
